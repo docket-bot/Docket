01107a8c5b2557ea14e3f96272ba90ea110649bc
Circuit <circuitsacul@icloud.com>
2022-06-04 13:35:40 -0400

    Add other events

--- bot/plugins/events.py
+++ bot/plugins/events.py
@@ -8,7 +8,45 @@ from bot.core.event import handle_event
 plugin = crescent.Plugin("custom-events")
 
 
+# message events
 @plugin.include
 @crescent.event
 async def on_message(event: hikari.GuildMessageCreateEvent) -> None:
+    await handle_event(event)
+
+
+@plugin.include
+@crescent.event
+async def on_message_delete(event: hikari.GuildMessageDeleteEvent) -> None:
+    await handle_event(event)
+
+
+@plugin.include
+@crescent.event
+async def on_message_edit(event: hikari.GuildMessageUpdateEvent) -> None:
+    await handle_event(event)
+
+
+@plugin.include
+@crescent.event
+async def on_message_bulk_delete(event: hikari.GuildBulkMessageDeleteEvent) -> None:
+    await handle_event(event)
+
+
+# reaction events
+@plugin.include
+@crescent.event
+async def on_reaction_add(event: hikari.GuildReactionAddEvent) -> None:
+    await handle_event(event)
+
+
+@plugin.include
+@crescent.event
+async def on_reaction_remove(event: hikari.GuildReactionDeleteEvent) -> None:
+    await handle_event(event)
+
+
+@plugin.include
+@crescent.event
+async def on_reaction_clear(event: hikari.GuildReactionDeleteAllEvent) -> None:
     await handle_event(event)
