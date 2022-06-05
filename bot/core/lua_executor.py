c68a30dc7b2774b577170fc6859c4c6cc6fbfb06
Circuit <circuitsacul@icloud.com>
2022-06-05 17:38:12 -0400

    Add tonumber builtin

--- bot/core/lua_executor.py
+++ bot/core/lua_executor.py
@@ -12,7 +12,7 @@ if TYPE_CHECKING:
     from bot.bot import Bot
 
 INITIAL_LUA = "function (code, env) pcall(load(code, nil, 't', env)) end"
-SAFE_BUILTINS = ["tostring"]
+SAFE_BUILTINS = ["tostring", "tonumber"]
 
 
 def get_env(bot: Bot, runtime: Any, guild: int) -> dict[str, Any]:
