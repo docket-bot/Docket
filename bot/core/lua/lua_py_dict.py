124da10832b0fe19c362f3718e71ccf8396ef043
Circuit <circuitsacul@icloud.com>
2022-06-05 19:36:03 -0400

    convert all ints to strings

--- bot/core/lua/lua_py_dict.py
+++ bot/core/lua/lua_py_dict.py
@@ -2,23 +2,12 @@ from __future__ import annotations
 
 from typing import Any
 
-MAX_LUA_INT = 982688721935675392
-
 
 class LuaPyDict(dict):  # type: ignore
     def __getitem__(self, key: str) -> Any:
         val = super().__getitem__(key)
         if isinstance(val, dict):
             return LuaPyDict(val)
-        if isinstance(val, int) and val > MAX_LUA_INT:
+        if isinstance(val, int):
             return str(val)
         return val
-
-    def __getattr__(self, key: str) -> Any:
-        return self.__getitem__(key)
-
-    def __setattr__(self, key: str, value: Any) -> None:
-        self.__setitem__(key, value)
-
-    def __delattr__(self, key: str) -> None:
-        self.__delitem__(key)
