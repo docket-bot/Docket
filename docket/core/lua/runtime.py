eb0ba235ca9bf3719946f9c93b1685766d30f6a3
Circuit <circuitsacul@icloud.com>
2022-06-07 22:58:16 -0400

    put lua boot in own file

--- docket/core/lua/runtime.py
+++ docket/core/lua/runtime.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import asyncio
+from pathlib import Path
 from typing import TYPE_CHECKING, Any, NoReturn
 
 import lupa
@@ -13,63 +14,8 @@ if TYPE_CHECKING:
     from docket.bot import Docket
 
 
-# LUA_BOOT was provided by the developer of cleanerbot.xyz, with permission
-LUA_BOOT = """
-local debug_sethook = debug.sethook
-local coroutine = coroutine
-local error = error
-local setmetatable = setmetatable
-local unpack = unpack or table.unpack
-local script, cycle_limit
-local load = load
-local pairs = pairs
-local type = type
-_G.load = function(chunk, chunkname, mode, env)
-    if mode ~= "t" then return nil, "mode must be 't'" end
-    return load(chunk, chunkname, "t", env)
-end
-local function safe_call(fn, ...)
-    if cycle_limit == nil then
-        error("no cycle limit, call set_cycle_limit")
-    end
-    local coro = coroutine.create(fn)
-    local limit_exceeded = false
-    debug_sethook(coro, function()
-        limit_exceeded = true
-        debug_sethook(function() error("reached cycle limit") end, "cr", 1)
-        error("reached cycle limit")
-    end, "", cycle_limit)
-    local result = {coroutine.resume(coro, ...)}
-    if limit_exceeded then
-        error("reached cycle limit")
-    end
-    for _, v in pairs(result) do
-        if type(v) == "table" then
-            setmetatable(v, {})
-        end
-    end
-    if not result[1] then
-        error(unpack(result, 2))
-    end
-    return unpack(result, 2)
-end
-return {
-    set_cycle_limit = function(limit)
-        cycle_limit = limit
-    end,
-    set_script = function(source)
-        local fn, err = load(source, "<worker>", "t")
-        if fn == nil then
-            error(err)
-        end
-        script = safe_call(fn)
-    end,
-    call = function(...)
-        return safe_call(script, ...)
-    end,
-}
-"""
-
+with open(Path("docket/core/lua/boot.lua"), "r") as f:
+    LUA_BOOT = f.read()
 LUA_WHITELIST: list[str] = []
 
 
